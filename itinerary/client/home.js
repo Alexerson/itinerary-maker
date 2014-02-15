c55d90d4b9fd4cb62bacf74e01eec51d91096b76
Angela Zhang <zhangela@mit.edu>
2014-02-15 17:34:31 +0400

    added destinations to itinuerary

--- itinerary/client/home.js
+++ itinerary/client/home.js
@@ -1,6 +1,9 @@
 Template.home.events({
     "click button": function(event, template) {
         var city = template.find("input").value;
+        Itineraries.insert({user: Meteor.user(), city: city, destinations: []}, function(error, id) {
+          Session.set("currentItineraryID", id);
+        });
         Router.go('/planner/' + city);
     }
 
---
+++ itinerary/client/views/destinations.js
@@ -0,0 +1,12 @@
+Template.destinations.events({
+    "change input[type=checkbox]": function(event, template) {
+        var checked = event.target.checked;
+        var currentItineraryID = Session.get("currentItineraryID");
+
+        if (checked) {
+          Itineraries.update(currentItineraryID, { $push: { destinations: this }});
+        } else {
+          Itineraries.update(currentItineraryID, { $pull: { destinations: this }});
+        }
+    }
+});
