d68b1db44291fab5847e9ee5921e646c41db217b
Angela Zhang <zhangela@mit.edu>
2014-02-15 20:50:51 +0400

    itineraries are saved in url, checkboxes now stay checked if destination in itinerary

--- itinerary/client/views/destinations.js
+++ itinerary/client/views/destinations.js
@@ -3,10 +3,32 @@ Template.destinations.events({
         var checked = event.target.checked;
         var currentItineraryID = Session.get("currentItineraryID");
 
+        if (currentItineraryID === null) {
+          console.log("ITINERARY ID IS NULL");
+        }
+
         if (checked) {
           Itineraries.update(currentItineraryID, { $push: { destinations: this }});
         } else {
           Itineraries.update(currentItineraryID, { $pull: { destinations: this }});
         }
     }
+});
+
+Template.destinations.helpers({
+  destinations: function () {
+    return Destinations.find({city: Session.get("currentCity")});
+  },
+  itinerary: function() {
+    return Itineraries.findOne(Session.get("currentItineraryID"));
+  },
+  destinationInItinerary: function(destination) {
+    var itinerary = Itineraries.findOne(Session.get("currentItineraryID"));
+    foursquare_ids = _.map(itinerary.destinations, function(item) {return item.foursquare_id});
+    if (foursquare_ids.indexOf(destination.foursquare_id) >= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 });
